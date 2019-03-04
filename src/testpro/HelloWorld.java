4e8e0d4965a86492e2f2a654332d9fafcb6cd600
OmKumarGaurav <gauravomj@gmail.com>
2019-03-04 07:27:37 +0530

    initial commit

--- src/testpro/HelloWorld.java
+++ src/testpro/HelloWorld.java
@@ -3,5 +3,7 @@ package testpro;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("Good morning");
+
     }
 }
